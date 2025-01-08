ec8839a487c3f93135d34cbbe7675e4f82547bed
Swap-24 <168347749+Swap-24@users.noreply.github.com>
2025-01-08 20:00:46 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('#darkmode-toggle').click(function() {
+        $('body').toggleClass('darkmode');
+    });
+    $('#simon-card').click(function() {
+        window.open('https://swap-24.github.io/simon-says/', '_blank');
+    });
+    $('#dog-card').click(function() {
+        window.open('https://swap-24.github.io/dog-tinder/', '_blank');
+    });
+    $('#drum-card').click(function() {
+        window.open('https://swap-24.github.io/drum-kit/', '_blank');
+    });
+});
